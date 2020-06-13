b440a066cec95e56e3d27fe793d357f22c09842d
Felipe Rocha <rochadsf@gmail.com>
2020-06-13 01:00:21 -0300

    Adiciona texto com clique no Menu

--- menu-guia.js
+++ menu-guia.js
@@ -1,5 +1,35 @@
 var indiceGuia = document.querySelector(".menu-painel");
 
+var tipoGuia = document.querySelector(".botao-produto-vtpro");
+
+
+
 indiceGuia.addEventListener("click", function(){
-    console.log("click");
+    
+    var pegaTexto = document.querySelector(".plataforma-otimiza");
+
+    var incluiTexto = document.querySelector(".texto-exibido");
+    
+    incluiTexto.innerHTML = pegaTexto.textContent;
+    
 });
+
+
+// var textoGuia = document.querySelector(".principal");
+// var adicionaTexto = document.querySelector(".vt-pro");
+// textoGuia.innerHTML = adicionaTexto.innerHTML;
+// console.log(textoGuia.innerHTML);
+
+// tipoGuia.addEventListener("click", function(){
+//   var textoGuia = document.querySelector(".texto-guia");
+//     var adicionaTexto = document.querySelector(".vt-pro");
+//     textoGuia.innerHTML = adicionaTexto.innerHTML;
+//     console.log(textoGuia.innerHTML);
+// })
+    
+
+
+
+// var encontraItem = document.querySelector(".painel-beneficiario");
+// window.scrollTo(encontraItem.scrollWidth,encontraItem.scrollHeight);
+// console.log(encontraItem.innerHTML);
