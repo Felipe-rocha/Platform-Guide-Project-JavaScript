b4577fc89b262f0abb9aa6c97b0b4c889fd6dc61
Felipe Rocha <rochadsf@gmail.com>
2020-06-14 22:46:11 -0300

    Identificação e Mudança de texto de acordo com o click no Menu

--- menu-guia.js
+++ menu-guia.js
@@ -1,18 +1,58 @@
-var indiceGuia = document.querySelector(".menu-painel");
+var painelGuia = document.querySelector(".menu-painel");
+var vtGuia = document.querySelector(".menu-vt");
+var cadastroGuia = document.querySelector(".menu-cadastros");
+var acessoGuia = document.querySelector(".menu-acesso");
+var suporteGuia = document.querySelector(".menu-suporte");
 
 var tipoGuia = document.querySelector(".botao-produto-vtpro");
 
 
 
-indiceGuia.addEventListener("click", function(){
-    
-    var pegaTexto = document.querySelector(".plataforma-otimiza");
+painelGuia.addEventListener("click", mudaTexto);
+vtGuia.addEventListener("click", mudaTexto);
+cadastroGuia.addEventListener("click", mudaTexto);
+acessoGuia.addEventListener("click", mudaTexto);
+suporteGuia.addEventListener("click", mudaTexto);
 
-    var incluiTexto = document.querySelector(".texto-exibido");
+
+
+
+function mudaTexto (){
     
-    incluiTexto.innerHTML = pegaTexto.textContent;
+    var pegaTexto = document.querySelector(".texto-exibido");    
     
-});
+    console.log(this);
+
+    if(this.textContent == "Painel Inicial"){
+        var textoMenu = document.querySelector(".texto-1");
+        pegaTexto.innerHTML = textoMenu.textContent;
+        console.log(textoMenu);
+    };
+
+    if(this.textContent == "Vale Transporte"){
+        var textoMenu = document.querySelector(".texto-2");
+        pegaTexto.innerHTML = textoMenu.textContent;
+        console.log(textoMenu);
+    };
+    if(this.textContent == "Cadastros"){
+        var textoMenu = document.querySelector(".texto-3");
+        pegaTexto.innerHTML = textoMenu.textContent;
+        console.log(textoMenu);
+    };
+    if(this.textContent == "Acessos"){
+        var textoMenu = document.querySelector(".texto-4");
+        pegaTexto.innerHTML = textoMenu.textContent;
+        console.log(textoMenu);
+    };
+
+    if(this.textContent == "Suporte Online"){
+        var textoMenu = document.querySelector(".texto-5");
+        pegaTexto.innerHTML = textoMenu.textContent;
+        console.log(textoMenu);
+    };
+};
+
+
 
 
 // var textoGuia = document.querySelector(".principal");
